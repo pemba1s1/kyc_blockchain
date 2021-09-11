b8143d38eb5932aa2c33df79a094fdf191db89b5
pemba1s1 <norsangpemba@gmail.com>
2021-09-11 14:44:23 +0545

    Landing Page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,46 +1,12 @@
 import React, { Component } from "react";
-import Kyc from "../contracts/Kyc.json";
-import Web3 from 'web3'
-import NavBar from './NavBar'
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/Row"
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+import kyc from '../img/kyc.jpeg'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
-  async componentWillMount() {
-    await this.loadWeb3()
-    await this.loadBlockchainData()
-  }
-
-  async loadBlockchainData() {
-    //Declare Web3
-    const web3 = window.web3
-    //Load account
-    const accounts = await web3.eth.getAccounts()
-    this.setState({account : accounts[0]})
-    //Network ID
-    const networkId = await web3.eth.net.getId()
-    const networkData = Kyc.networks[networkId]
-    //IF got connection, get data from contracts
-    if(networkData){
-      const kyc = new web3.eth.Contract(Kyc.abi, networkData.address)
-    }else{
-      window.alert('KYC contract not deployed on this network')
-    }
-      
-
-  }
-
-  async loadWeb3() {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
-    }
-  }
 
   constructor(props){
     super(props);
@@ -51,7 +17,26 @@ class App extends Component {
 
   render() {
     return (
-      <NavBar account={this.state.account}></NavBar>
+      <Container fluid style={{height: '100vh'}}>
+        <Row style={{height: "100%" ,textAlign: "center"}}>
+          <Col style={{backgroundImage:`url(${kyc})`,backgroundSize:'Cover',backgroundRepeat:'no-repeat'}}>
+
+          </Col>
+          <Col sm={5} style={{backgroundColor:"white"}}>
+            <Row style={{height: "60%",paddingTop:"60px"}}>
+              <Col><h1><b>KYC Blockchain</b></h1></Col>
+            </Row>
+            <Row style={{height: "40%" ,paddingTop:"60px"}}>
+              <Col >
+                <h2><Button variant="outline-dark" size= "lg" href="/organization" style={{borderRadius:"10px"}}>Organization</Button></h2>
+              </Col>
+              <Col >
+                <h2><Button variant="outline-dark" size="lg" href="/customer" style={{borderRadius:"10px"}}>Customer</Button></h2>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }

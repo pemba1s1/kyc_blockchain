af6d1d953035763f12ef9c9e6b7e9883915cef12
pemba1s1 <norsangpemba@gmail.com>
2021-09-14 13:25:13 +0545

    customer input data in contract

--- contracts/Kyc.sol
+++ contracts/Kyc.sol
@@ -5,9 +5,11 @@ pragma experimental ABIEncoderV2;
 contract Kyc {
   address public admin;
   struct Customer {
-      uint uname;
-      string name;
-      string location;
+      address custAddress;
+      string jsonHash;
+      string photoHash;
+      string citizenship_front_hash;
+      string citizenship_back_hash;
       address[] organization;
       bool kycStatus;
   }
@@ -25,25 +27,25 @@ contract Kyc {
   
   struct kycRequestList{
       uint req_count;
-      uint uname;
+      address custAddress;
   }
 
   constructor() public {
       admin = msg.sender;
   }
 
-  mapping(uint => Customer) customers;
+  mapping(address => Customer) customers;
   mapping(address => Organization) organizations;
   mapping(address => kycRequest) kycrequestsbyorg;
 
   event orgAdded(string name,address ethAddress);
   event orgRemoved(address ethAddress);
-  event customerAdded(uint uname,string name,string location,bool kycStatus);
-  event customerUpdated(uint uname,string name,string location,bool kycStatus);
-  event requestAdded(uint reqid,address ethAddress, uint uname,bool isAllowed);
+  event customerAdded(address custAddress,bool kycStatus);
+  event customerUpdated(address custAddress,bool kycStatus);
+  event requestAdded(uint reqid,address ethAddress, address custAddress,bool isAllowed);
   event requestRemoved(uint reqid,address ethAddress);
-  event accessGiven(uint reqid,uint uname,address ethAddress,bool isAllowed);
-  event accessRevoked(uint uname,address ethAddress);
+  event accessGiven(uint reqid,address custAddress,address ethAddress,bool isAllowed);
+  event accessRevoked(address custAddress,address ethAddress);
 
 // Checks whether the requestor is admin
   modifier isAdmin {
@@ -64,30 +66,30 @@ contract Kyc {
   }
 
 //Check if Org has access to the user KYC
-  function findOrg(uint _uname,address ethAddress) internal view returns(bool){
+  function findOrg(address _custaddress,address ethAddress) internal view returns(bool){
       uint i = 0;
-          for(i;i<customers[_uname].organization.length;i++){
-              if(customers[_uname].organization[i]==ethAddress){
+          for(i;i<customers[_custaddress].organization.length;i++){
+              if(customers[_custaddress].organization[i]==ethAddress){
                   return true;
               }
           }
   }
 
 //Find the index of Org in KYC.organization array
-  function findOrgIndex(uint _uname,address ethAddress) internal view returns(uint){
+  function findOrgIndex(address _custaddress,address ethAddress) internal view returns(uint){
       uint i = 0;
-      for(i;i<customers[_uname].organization.length;i++){
-            if(customers[_uname].organization[i]==ethAddress){
+      for(i;i<customers[_custaddress].organization.length;i++){
+            if(customers[_custaddress].organization[i]==ethAddress){
                 return i;
             }
     }
   }
 
 //Check if request exist 
-  function findRequest(uint _uname) internal view returns(bool){
+  function findRequest(address _custaddress) internal view returns(bool){
       uint i=0;
       for(i;i<kycrequestsbyorg[msg.sender].req_count;i++){
-          if(kycrequestsbyorg[msg.sender].kycrequestlist[i].uname==_uname){
+          if(kycrequestsbyorg[msg.sender].kycrequestlist[i].custAddress==_custaddress){
               return true;
           }
       }
@@ -135,72 +137,80 @@ contract Kyc {
   }
 
 //Register User KYC by registered Orgs
-  function registerKYC(uint _uname , string memory _name, string memory _location,bool _kycStatus) public payable isOrgValid returns(bool){
-      require(customers[_uname].uname!=_uname,"User already exists");
-      customers[_uname].uname=_uname;
-      customers[_uname].name=_name;
-      customers[_uname].location=_location;
-      customers[_uname].kycStatus=_kycStatus;
-      requestKYC(_uname);
-      giveAccessKYC(kycrequestsbyorg[msg.sender].req_count -1,_uname,msg.sender,true);
-      emit customerAdded(_uname,_name,_location,_kycStatus);
+  function registerKYC(address _custaddress,string memory _jsonHash,string memory _photohash,string memory _citizenship_front_hash,string memory _citizenship_back_hash,bool _kycStatus) public payable isOrgValid returns(bool){
+      require(customers[_custaddress].custAddress!=_custaddress,"User already exists");
+      require(bytes(_jsonHash).length > 0);
+      require(bytes(_photohash).length > 0);
+      require(bytes(_citizenship_back_hash).length > 0);
+      require(bytes(_citizenship_front_hash).length > 0);
+      customers[_custaddress].custAddress=_custaddress;
+      customers[_custaddress].jsonHash=_jsonHash;
+      customers[_custaddress].photoHash=_photohash;
+      customers[_custaddress].citizenship_front_hash=_citizenship_front_hash;
+      customers[_custaddress].citizenship_back_hash=_citizenship_back_hash;
+      customers[_custaddress].kycStatus=_kycStatus;
+      requestKYC(_custaddress);
+      emit customerAdded(_custaddress,_kycStatus);
       return true;
   }
 
 //View User KYC by Authorized Orgs  
-  function viewKYC(uint _uname) public view returns(uint,string memory,string memory,bool,address[] memory,uint){
-      require(findOrg(_uname,msg.sender),"User hasnt given their consent");
-      return(customers[_uname].uname,customers[_uname].name,customers[_uname].location,customers[_uname].kycStatus,customers[_uname].organization,customers[_uname].organization.length);
+  function viewKYC(address _custaddress) public view returns(address,string memory,string memory,bool,address[] memory,string memory,string memory){
+      require(findOrg(_custaddress,msg.sender),"User hasnt given their consent");
+      return(customers[_custaddress].custAddress,customers[_custaddress].jsonHash,customers[_custaddress].photoHash,customers[_custaddress].kycStatus,customers[_custaddress].organization,customers[_custaddress].citizenship_front_hash,customers[_custaddress].citizenship_back_hash);
   }
 
 //Update User KYC by registered and authorized orgs
-  function updateKYC(uint _uname , string memory _name, string memory _location,bool _kycStatus) public isOrgValid returns(bool){
-      require(customers[_uname].uname==_uname,"User doesn't exist");
-      require(findOrg(_uname,msg.sender),"You dont have access to this user");
-      customers[_uname].uname=_uname;
-      customers[_uname].name=_name;
-      customers[_uname].location=_location;
-      customers[_uname].kycStatus=_kycStatus;
-      emit customerUpdated(_uname,_name,_location,_kycStatus);
+  function updateKYC(address _custaddress,string memory _jsonHash,string memory _photohash,string memory _citizenship_front_hash,string memory _citizenship_back_hash,bool _kycStatus) public isOrgValid returns(bool){
+      require(customers[_custaddress].custAddress==_custaddress,"User doesn't exist");
+      require(findOrg(_custaddress,msg.sender),"You dont have access to this user");
+      customers[_custaddress].custAddress=_custaddress;
+      customers[_custaddress].jsonHash=_jsonHash;
+      customers[_custaddress].photoHash=_photohash;
+      customers[_custaddress].citizenship_front_hash=_citizenship_front_hash;
+      customers[_custaddress].citizenship_back_hash=_citizenship_back_hash;
+      customers[_custaddress].kycStatus=_kycStatus;
+      emit customerUpdated(_custaddress,_kycStatus);
       return true;
   }
 
 //Remove User KYC by Orgs  
-  function removeKYC(uint _uname) public isOrgValid returns (bool){
-      require(customers[_uname].uname==_uname,"User doesn't exist");
-      require(findOrg(_uname,msg.sender),"You dont have access to this user");
-      uint i=findOrgIndex(_uname,msg.sender);
-      customers[_uname].organization[i]=customers[_uname].organization[customers[_uname].organization.length-1];
-      customers[_uname].organization.pop();
+  function removeKYC(address _custaddress) public isOrgValid returns (bool){
+      require(customers[_custaddress].custAddress==_custaddress,"User doesn't exist");
+      require(findOrg(_custaddress,msg.sender),"You dont have access to this user");
+      uint i=findOrgIndex(_custaddress,msg.sender);
+      customers[_custaddress].organization[i]=customers[_custaddress].organization[customers[_custaddress].organization.length-1];
+      customers[_custaddress].organization.pop();
   }
 
 //Request User KYC by Orgs  
-  function requestKYC(uint _uname) public isOrgValid returns(bool){
-      require(customers[_uname].uname==_uname,"Customer doesnt exists");
-      require(findOrg(_uname,msg.sender)==false,"Already have access");
-      require(findRequest(_uname)==false,"Request Already Exists");
-      kycrequestsbyorg[msg.sender].kycrequestlist.push(kycRequestList(kycrequestsbyorg[msg.sender].req_count,_uname));
+  function requestKYC(address _custaddress) public isOrgValid returns(bool){
+      require(customers[_custaddress].custAddress==_custaddress,"Customer doesnt exists");
+      require(findOrg(_custaddress,msg.sender)==false,"Already have access");
+      require(findRequest(_custaddress)==false,"Request Already Exists");
+      kycrequestsbyorg[msg.sender].kycrequestlist.push(kycRequestList(kycrequestsbyorg[msg.sender].req_count,_custaddress));
       kycrequestsbyorg[msg.sender].req_count++;
-      emit requestAdded(kycrequestsbyorg[msg.sender].req_count-1,msg.sender,_uname,false);
+      emit requestAdded(kycrequestsbyorg[msg.sender].req_count-1,msg.sender,_custaddress,false);
       return true;
   }
 
 //Delete User KYC Request By Orgs 
-  function deleteRequest(uint _reqcount) public isOrgValid{
-      require(kycrequestsbyorg[msg.sender].kycrequestlist[_reqcount].req_count==_reqcount,"Request doesnt exist");
-      kycrequestsbyorg[msg.sender].kycrequestlist[_reqcount]=kycrequestsbyorg[msg.sender].kycrequestlist[kycrequestsbyorg[msg.sender].kycrequestlist.length-1];
-      kycrequestsbyorg[msg.sender].kycrequestlist.pop();
-      if(kycrequestsbyorg[msg.sender].req_count>1){
-          kycrequestsbyorg[msg.sender].kycrequestlist[_reqcount].req_count=_reqcount;
+  function deleteRequest(uint _reqcount,address _ethAddress) public{
+      require(organizations[_ethAddress].ethAddress==_ethAddress,"This particular organizations doesnt exist");
+      require(kycrequestsbyorg[_ethAddress].kycrequestlist[_reqcount].req_count==_reqcount,"Request doesnt exist");
+      kycrequestsbyorg[_ethAddress].kycrequestlist[_reqcount]=kycrequestsbyorg[_ethAddress].kycrequestlist[kycrequestsbyorg[_ethAddress].kycrequestlist.length-1];
+      kycrequestsbyorg[_ethAddress].kycrequestlist.pop();
+      if(kycrequestsbyorg[_ethAddress].req_count>1){
+          kycrequestsbyorg[_ethAddress].kycrequestlist[_reqcount].req_count=_reqcount;
       }
-      kycrequestsbyorg[msg.sender].req_count--;
-      emit requestRemoved(_reqcount,msg.sender);
+      kycrequestsbyorg[_ethAddress].req_count--;
+      emit requestRemoved(_reqcount,_ethAddress);
   }
 
 //View specific request by Orgs
-  function viewRequest(uint _reqcount) public isOrgValid view returns(uint, uint){
+  function viewRequest(uint _reqcount) public isOrgValid view returns(uint, address){
       require(kycrequestsbyorg[msg.sender].ethAddress==msg.sender,"You are not authorized");
-      return(kycrequestsbyorg[msg.sender].kycrequestlist[_reqcount].req_count,kycrequestsbyorg[msg.sender].kycrequestlist[_reqcount].uname);
+      return(kycrequestsbyorg[msg.sender].kycrequestlist[_reqcount].req_count,kycrequestsbyorg[msg.sender].kycrequestlist[_reqcount].custAddress);
   }
 
 //Return Array of all pending request of the orgs
@@ -216,26 +226,27 @@ contract Kyc {
   }
 
 //Give User KYC access to Org
-  function giveAccessKYC(uint _reqcount,uint _uname,address _ethAddress,bool _isAllowed) public returns(bool){
+  function giveAccessKYC(uint _reqcount,address _ethAddress,bool _isAllowed) public returns(bool){
+      require(customers[msg.sender].custAddress==msg.sender,"You dont have authority to give access");
       require(kycrequestsbyorg[_ethAddress].kycrequestlist[_reqcount].req_count==_reqcount,"Requests not found");
       if(_isAllowed == true){
-          if(findOrg(_uname,msg.sender) == false){
-              customers[_uname].organization.push(_ethAddress);
+          if(findOrg(msg.sender,_ethAddress) == false){
+              customers[msg.sender].organization.push(_ethAddress);
           }
-          deleteRequest(_reqcount);
-          emit accessGiven(_reqcount,_uname,_ethAddress,_isAllowed);
+          deleteRequest(_reqcount,_ethAddress);
+          emit accessGiven(_reqcount,msg.sender,_ethAddress,_isAllowed);
       }
       
   }
 
 //Revoke User KYC access of Orgs
-  function revokeAccessKYC(uint _uname,address _ethAddress) public returns(bool){
-      require(customers[_uname].uname==_uname,"User doesn't exist");
-      require(findOrg(_uname,_ethAddress),"You dont have access to this user");
-      uint i=findOrgIndex(_uname,_ethAddress);
-      customers[_uname].organization[i]=customers[_uname].organization[customers[_uname].organization.length-1];
-      customers[_uname].organization.pop();
-      emit accessRevoked(_uname,_ethAddress);
+  function revokeAccessKYC(address _ethAddress) public returns(bool){
+      require(customers[msg.sender].custAddress==msg.sender,"You dont have authority to revoke access");
+      require(findOrg(msg.sender,_ethAddress),"You dont have access to this user");
+      uint i=findOrgIndex(msg.sender,_ethAddress);
+      customers[msg.sender].organization[i]=customers[msg.sender].organization[customers[msg.sender].organization.length-1];
+      customers[msg.sender].organization.pop();
+      emit accessRevoked(msg.sender,_ethAddress);
   }
   
 }
--- test/Kyc.test.js
+++ test/Kyc.test.js
@@ -19,26 +19,41 @@ contract('KYC',accounts=>{
         })
     })
 
-    describe("registerKYC() , giveAccessKYC(), viewRequest() , requestKYC(),deleteRequest() & viewKYC()",async() => {
-        it('KYC added, request added, KYC access given, KYC viewed, Request viewed,Request Deleted',async()=>{
-            await kyc.registerKYC(1,'Pemba','Kapan',true,{from : '0xD0B5BAED90404ADaB3163bFFa9Ba6543C9301025'})
-            const name = await kyc.viewKYC(1,{from:'0xD0B5BAED90404ADaB3163bFFa9Ba6543C9301025'})
-            assert.equal(name[1],'Pemba')
+    describe("registerKYC() &  requestKYC()",async() => {
+        it('KYC added, request added',async()=>{
+            await kyc.registerKYC('0x1e84892176f6aE044E620C61133Ce5532166d8DC','json','photo','front','back',true,{from : '0xD0B5BAED90404ADaB3163bFFa9Ba6543C9301025'})
+            
+        })
+    })
+
+    describe(" giveAccessKYC(), viewRequest() ,deleteRequest() & viewKYC()",async()=>{
+        it("Access given, request viewed,request deleted and kyc viewed",async()=>{
+            await kyc.giveAccessKYC(0,'0xD0B5BAED90404ADaB3163bFFa9Ba6543C9301025',true,{from : '0x1e84892176f6aE044E620C61133Ce5532166d8DC'})
+            const name = await kyc.viewKYC('0x1e84892176f6aE044E620C61133Ce5532166d8DC',{from : '0xD0B5BAED90404ADaB3163bFFa9Ba6543C9301025'})
+            assert.equal(name[1],'json')
             assert.equal(name[4][0],'0xD0B5BAED90404ADaB3163bFFa9Ba6543C9301025')
         })
     })
 
     describe("updateKYC() & viewKYC()",async() => {
         it("KYC updated",async()=>{
-            await kyc.updateKYC(1,'Updated','Kapan',true,{from : '0xD0B5BAED90404ADaB3163bFFa9Ba6543C9301025'})
-            const name = await kyc.viewKYC(1,{from:'0xD0B5BAED90404ADaB3163bFFa9Ba6543C9301025'})
-            assert.equal(name[1],'Updated')
+            await kyc.updateKYC('0x1e84892176f6aE044E620C61133Ce5532166d8DC','jsonupdated','photo','front','back',true,{from : '0xD0B5BAED90404ADaB3163bFFa9Ba6543C9301025'})
+            const name = await kyc.viewKYC('0x1e84892176f6aE044E620C61133Ce5532166d8DC',{from:'0xD0B5BAED90404ADaB3163bFFa9Ba6543C9301025'})
+            assert.equal(name[1],'jsonupdated')
         })
     })
 
     describe("revokeAccess()",async()=>{
-        it("kyc removed and access revoked",async()=>{
-            await kyc.revokeAccessKYC(1,'0xD0B5BAED90404ADaB3163bFFa9Ba6543C9301025',{from:'0xD0B5BAED90404ADaB3163bFFa9Ba6543C9301025'})
+        it("access revoked",async()=>{
+            await kyc.revokeAccessKYC('0xD0B5BAED90404ADaB3163bFFa9Ba6543C9301025',{from:'0x1e84892176f6aE044E620C61133Ce5532166d8DC'})
+        })
+    })
+
+    describe("removeKYC()",async()=>{
+        it("kyc removed",async()=>{
+            await kyc.requestKYC('0x1e84892176f6aE044E620C61133Ce5532166d8DC',{from:'0xD0B5BAED90404ADaB3163bFFa9Ba6543C9301025'})
+            await kyc.giveAccessKYC(0,'0xD0B5BAED90404ADaB3163bFFa9Ba6543C9301025',true,{from : '0x1e84892176f6aE044E620C61133Ce5532166d8DC'})
+            await kyc.removeKYC('0x1e84892176f6aE044E620C61133Ce5532166d8DC',{from :'0xD0B5BAED90404ADaB3163bFFa9Ba6543C9301025'})
         })
     })
 
